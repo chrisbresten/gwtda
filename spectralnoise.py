7659c9e477beb7995495a479f0d1df72fcd415b1
chris <chris@minds>
2022-03-29 17:02:49 +0900

    fixed

--- spectralnoise.py
+++ spectralnoise.py
@@ -2,62 +2,44 @@ import numpy as np
 
 
 class SpectralPerterb:  # haha
-    def __init__(self, spectra_means=None, specta_variance=None, N=None, mean=None):
-        """doesnt need any initial input, but the state(means and variances as an array_like) can be set so that a saved or externally calculated or synthetic distribution can be used"""
-        self.target_spectra_means = spectra_means
-        self.target_spectra_variance = specta_variance
-        self.N = N
-        if mean is None:
-            self.u = 1
-        else:
-            self.u = mean
+    def __init__(self):
+        pass
 
-    def fit(self, signals, skip=None, M=None):
+    def fit(self, signals, skip=None):
         """learns the M average highst FFT coefficients, from list in positional argument 1, skipping signals[j] where skip[j]=True"""
         N = len(signals[0])
         Nsig = len(signals)
-        u = signals.mean()
         if skip is None:
             skip = [False] * Nsig
         elif len(skip) != Nsig:
             raise ValueError("dimension mismathc")
-        if M is None:
-            M = N
-        H = np.zeros((Nsig, int(N / 2) + 1), dtype=np.complex128)
+        H = np.zeros((Nsig, int(N / 2 + 1)))
         for j, s in enumerate(signals):
-            if not skip[j]:  # wana look at the spectra of the noise
-                H[j, 0::] = np.fft.rfft(s, norm="ortho")
+            if not skip[
+                j
+            ]:  # wana look at the spectra of the noise, not the positive detected signals
+                H[j, :] = (2 / np.sqrt(N)) * np.fft.rfft(s).real
         self.target_spectra_means = np.mean(H, 1)
-        self.target_spectra_variance = np.var(H, 1)
+        self.target_spectra_variance = np.sqrt(np.var(H, 1))*np.sqrt(Nsig)
         self.N = N
-        self.u = u
-        ranks = np.argsort(np.mean(np.abs(H), 1))
-        self.target_spectra_variance[
-            ranks[0:-M]
-        ] = 0  # makes the perterbation 0 for everything but ther top N means
+        self.H = H
+        self.Nsig = Nsig
 
-    def perterb(self, signal, c=0.05):
+    def _perterb(self, signal, c=0.05):
         """perterbs the fourier coefficients of an iterable list/array/tupple of signals(as numpy arrays)
         proportional to c=0.05(default) with random samples from a normal distribution for each fourier coefficient
         with the mean and variance in self.target_spectra_variance and self.target_spectra_mean. It is a statistically-speaking, a unitary operation"""
         out = []
-        for s in signal:
-            n = len(s)
-            print(s.shape)
+        n = self.N
+        for j in range(self.Nsig):
             out.append(
-                 np.fft.irfft(
-                    np.fft.rfft(s,norm='ortho')*(1-c)
-                    + np.random.normal(
+                H[j, :]
+                + np.fft.irfft(
+                    np.random.normal(
                         self.target_spectra_means.real,
-                        np.sqrt(self.target_spectra_variance.real),
-                    )
-                    + np.random.normal(
-                        self.target_spectra_means.imag,
-                        np.sqrt(self.target_spectra_variance).imag,
-                    )
-                    * np.complex128(complex(0, 1)),
+                        self.target_spectra_variance.real,
+                    ),
                     n=n,
-                    norm="ortho",
                 )
             )
         return np.array(out)
@@ -66,18 +48,10 @@ class SpectralPerterb:  # haha
         """outputs a inverse-fft reconstructed signal randomly from the known means and variances"""
         if n is None:
             n = self.N
-        return np.fft.irfft(
-            c
-            * np.random.normal(
+        return (np.sqrt(n) / 2) * np.fft.irfft(
+            np.random.normal(
                 self.target_spectra_means.real,
-                np.sqrt(self.target_spectra_variance.real),
-            )
-            + c
-            * np.random.normal(
-                self.target_spectra_means.imag,
-                np.sqrt(self.target_spectra_variance).imag,
-            )
-            * np.complex128(complex(0, 1)),
+                self.target_spectra_variance.real,
+            ),
             n=n,
-            norm="ortho",
         )
