bc6a19d3464ddaca1681234865c6a4f44c3fa1c5
chris <chris@minds>
2022-03-29 14:29:50 +0900

    .

--- spectralnoise.py
+++ spectralnoise.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class SpectralPerterbator:  # haha
+class SpectralPerterb:  # haha
     def __init__(self, spectra_means=None, specta_variance=None, N=None, mean=None):
         """doesnt need any initial input, but the state(means and variances as an array_like) can be set so that a saved or externally calculated or synthetic distribution can be used"""
         self.target_spectra_means = spectra_means
@@ -23,7 +23,7 @@ class SpectralPerterbator:  # haha
             raise ValueError("dimension mismathc")
         if M is None:
             M = N
-        H = np.zeros((Nsig, int(N/2)+1), dtype=np.complex128)
+        H = np.zeros((Nsig, int(N / 2) + 1), dtype=np.complex128)
         for j, s in enumerate(signals):
             if not skip[j]:  # wana look at the spectra of the noise
                 H[j, 0::] = np.fft.rfft(s, norm="ortho")
@@ -31,7 +31,7 @@ class SpectralPerterbator:  # haha
         self.target_spectra_variance = np.var(H, 1)
         self.N = N
         self.u = u
-        ranks = np.argsort(np.mean(H.abs, 1))
+        ranks = np.argsort(np.mean(np.abs(H), 1))
         self.target_spectra_variance[
             ranks[0:-M]
         ] = 0  # makes the perterbation 0 for everything but ther top N means
@@ -42,13 +42,12 @@ class SpectralPerterbator:  # haha
         with the mean and variance in self.target_spectra_variance and self.target_spectra_mean. It is a statistically-speaking, a unitary operation"""
         out = []
         for s in signal:
-            S = np.array(np.ravel(s))
-            n = len(S)
+            n = len(s)
+            print(s.shape)
             out.append(
-                S * (1 - c)
-                + c
-                * np.fft.irfft(
-                    np.random.normal(
+                 np.fft.irfft(
+                    np.fft.rfft(s,norm='ortho')*(1-c)
+                    + np.random.normal(
                         self.target_spectra_means.real,
                         np.sqrt(self.target_spectra_variance.real),
                     )
@@ -67,9 +66,18 @@ class SpectralPerterbator:  # haha
         """outputs a inverse-fft reconstructed signal randomly from the known means and variances"""
         if n is None:
             n = self.N
-        return self.u * np.fft.irfft(
-            np.random.normal(
-                self.target_spectra_means, np.sqrt(self.target_spectra_variance)
-            ),
+        return np.fft.irfft(
+            c
+            * np.random.normal(
+                self.target_spectra_means.real,
+                np.sqrt(self.target_spectra_variance.real),
+            )
+            + c
+            * np.random.normal(
+                self.target_spectra_means.imag,
+                np.sqrt(self.target_spectra_variance).imag,
+            )
+            * np.complex128(complex(0, 1)),
             n=n,
+            norm="ortho",
         )
