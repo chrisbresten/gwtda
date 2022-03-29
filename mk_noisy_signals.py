78b34cd37eac279ee8494beaf74e6c3daff715dd
chris <chris@minds>
2022-03-30 08:38:51 +0900

    looks ok

--- mk_noisy_signals.py
+++ mk_noisy_signals.py
@@ -1,4 +1,4 @@
-import signals
+import signal_sources
 import noise
 
 import sys
@@ -15,29 +15,31 @@ try:
     if len(sys.argv) > 3:
         Ndattotal = int(sys.argv[3])
 except (IndexError, ValueError):
-    SystemExit(        f"Usage {sys.argv[0]} <signal type gw | chirps | ligo > <noise ligo | whitenoise> <Ndata> \n whre (optional) Ndata is total elements to  create"    )
+    SystemExit(
+        f"Usage {sys.argv[0]} <signal type gw | chirps | ligo > <noise ligo | whitenoise> <Ndata> \n whre (optional) Ndata is total elements to  create"
+    )
 
 try:
-    fun = signals.legend[signal_type]
+    signal_source_fun = signal_sources.legend[signal_type]
     nfun = noise.legend[noise_type]
 except:
     sigt = str(list(signals.legend.keys()))
     SystemExit(f"unknown signal type {signal_type} not in {sigt}")
 
-signals = fun()
+signals = signal_source_fun()
 Nsig = len(signals)
 N = len(signals[0])
-ncoeff = 0.4
+ncoeff = 0.4 #coefficient that noise is scaled by
+p = 0.5 #proportion of elements with a signal present
+Nwindow = 200  # sliding window size
+
+
+y = np.zeros((Ndattotal, Nsig))
+psignals = []
 x = []
 xsig = []
 ybin = []
 pcloud = []
-maxs = 0.0
-k = 0
-p = 0.5
-Nwindow = 200  # sliding window size
-y = np.zeros((Ndattotal, Nsig))
-psignals = []
 
 for j in range(Ndattotal):
     yn = np.random.randint(Nsig)
@@ -45,28 +47,28 @@ for j in range(Ndattotal):
         signal = signals[yn]
         ybin.append([1, 0])
     else:
-        signal = signals[yn] * 0
+        signal = np.zeros(signals[yn].shape)
         ybin.append([0, 1])
     for kk in range(Nsig):
         y[j, kk] = int(yn == kk)  # turn to binary vector len n for n sig
     psignals.append(nfun(signal, Npad, ncoeff))
 
-for j,rawsig in enumerate(psignals):
+for j, rawsig in enumerate(psignals):
     slidez = slidend(rawsig / np.abs(rawsig).max(), Nwindow)
     threed = dimred3(slidez)
     xsig.append(rawsig.copy())
     pcloud.append(threed.copy())
     if j % (int(Ndattotal / 100)) == 1:
         jj = int((100 * j) / Ndattotal)
-        print("%s            " % (jj,))
+        print("%s   %s    " % (jj,'%'),end="\r")
 
 
 outfile = "%s_signal_sliding_windowN%d_%d.npy" % (
-    name,
+    signal_type + "_" + noise_type + "_" + str(ncoeff),
     Ndattotal,
     np.random.randint(5, 5000),
 )
 np.save(
     outfile,
-    (xsig, ybin, pcloud, Npad, ncoeff, Nwindow, Ndattotal, N, signals, Nsig, Nsamp, y),
+    (xsig, ybin, pcloud, Npad, ncoeff, Nwindow, Ndattotal, N, signals, Nsig, y),
 )
