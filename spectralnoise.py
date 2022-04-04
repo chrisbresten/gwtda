0da6d645a993ad0894298419b8c65bb90fe19941
chris <chris@minds>
2022-04-04 11:31:36 +0900

    fixed noise gen, new viz

--- spectralnoise.py
+++ spectralnoise.py
@@ -6,12 +6,13 @@ try:
 except:
     print("gwtools not installed, cant calculate SNR")
 
+
 class SpectralPerterb:  # haha
     def __init__(self, means=None, std=None, N=None):
         """can initialize with synthetic or saved state"""
         if means is not None and std is not None:
-            self.target_spectra_std = std 
-            self.target_spectra_means = means
+            self.stds = std
+            self.means = means
 
     def fit(self, signals, skip=None):
         """calculates and saves the means and stds of the discrete cosine
@@ -26,78 +27,97 @@ class SpectralPerterb:  # haha
         H = np.zeros((Nsig, int(N / 2 + 1)))
         for j, s in enumerate(signals):
             if not skip[j]:
-                H[j, :] = (2 / np.sqrt(N)) * np.fft.rfft(s).real
-        self.target_spectra_means = np.mean(H, 0)
-        self.target_spectra_std = np.std(H, 0)
-        self.target_spectra_var = (np.sqrt(Nsig))*np.var(H, 0)
+                H[j, :] = (1 / np.sqrt(N)) * np.fft.rfft(s)
+        self.means = np.mean(H, 0)
+        self.stds = np.std(H, 0)
         self.N = N
         self.H = H
         self.Nsig = Nsig
 
-    def perterb(self, signal, c=0.05):
+    def perterb(self, signals, c=0.05):
         """perterbs the fourier coefficients of an iterable list/array/tupple
         of signals(as numpy arrays) proportional to c with random samples from
         a normal distribution for each fourier coefficient with the mean and
         std in self.target_spectra_std and self.target_spectra_mean.
         It is a statistically-speaking, a unitary operation"""
-        out = signal.copy() * (1 - c)
-        for j, o in enumerate(out):
-            out[j] += self.synth(o.size) * c
+        out = []
+        for s in signals:
+            out.append(
+                np.sqrt(self.N)
+                * np.fft.irfft(
+                    (1 / np.sqrt(self.N))
+                    * np.fft.rfft(s)
+                    * np.random.normal(
+                        np.ones(np.shape(self.means)),
+                        (self.stds * (c / 2)),
+                    )
+                )
+            )
         return out
 
-    def synth(self, n=None):
+    def perterb_white(self, signals, c=0.05):
+        """perterbs the fourier coefficients of an iterable list/array/tupple
+        of signals(as numpy arrays) proportional to c with random samples from
+        a normal distribution for each fourier coefficient with the mean and
+        std in self.target_spectra_std and self.target_spectra_mean.
+        It is a statistically-speaking, a unitary operation"""
+        out = []
+        for s in signals:
+            out.append(
+                np.sqrt(self.N)
+                * np.fft.irfft(
+                    (1 / np.sqrt(self.N))
+                    * np.fft.rfft(s)
+                    * np.random.normal(1, c / 2, int(self.N / 2 + 1))
+                )
+            )
+        return out
+
+    def synth(self, n=None, means=None, stds=None):
         """outputs a inverse-fft reconstructed signal randomly from the known
         means and stds, specified length defeaulting to training length"""
+        if means is None:
+            means = self.means
+        if stds is None:
+            stds = self.stds
+
         if n is None:
             n = self.N
-        return (np.sqrt(n) / 2) * np.fft.irfft(
+        return (np.sqrt(n)) * np.fft.irfft(
             np.random.normal(
-                self.target_spectra_means.real,
-                self.target_spectra_std,
-            ),
-            n=n,
+                means,
+                stds,
+            )
         )
 
     def synth_white(self, n=None):
         """synths white noise scaled according to whats been fit"""
-        if n is None:
-            n = self.N
-        mu = np.mean(self.target_spectra_means.real)
-        v = self.target_spectra_std.mean()
-        return (np.sqrt(n) / 2) * np.fft.irfft(
-            np.random.normal(
-                np.ones(np.shape(self.target_spectra_means)) * mu,
-                np.ones(np.shape(self.target_spectra_std)) * v,
-            ),
+        return self.synth(
             n=n,
+            means=np.mean(self.means) * np.ones(np.shape(self.means)),
+            stds=np.mean(self.stds),
         )
 
-    def perterb_white(self, signal, c=0.05):
-        """perterbs equivalently, only with white noise rather than the custom
-        noise, but on the same scale"""
-        out = signal.copy() * (1 - c)
-        for j, o in enumerate(out):
-            out[j] += self.synth_white(o.size) * c
-        return out
-
-    def _snr(self,signal, signal_with_noise,freq=4096):
+    def snr(self, signal, signal_with_noise, freq=4096):
         """calculate snr of resulting signal based off of noise statistics used to generate it"""
         N = len(signal)
-        dt = 1/freq
-        WaveformLength = dt*N
+        dt = 1 / freq
+        WaveformLength = dt * N
         freqs, hFreq = gwutils.freqDomainWaveform(signal, dt)
         freqs_noise, nFreq = gwutils.freqDomainWaveform(signal_with_noise, dt)
         df = 1 / WaveformLength
-        
-        Sn =  np.pad(self.target_spectra_var,(0,N-len(self.target_spectra_var)),mode='symmetric')
+        Sn = np.pad(
+            (np.sqrt(N)) * self.stds ** 2.0,
+            (0, N - len(self.stds)),
+            mode="symmetric",
+        )
         numerator = np.real(
-            mismatch.inner_complex(hFreq, nFreq, psd=Sn, df=df, ligo=True, pos_freq=False)
+            mismatch.inner_complex(
+                hFreq, nFreq, psd=Sn, df=df, ligo=True, pos_freq=False
+            )
         )
         denominator = np.sqrt(
             mismatch.inner(hFreq, hFreq, psd=Sn, df=df, ligo=True, pos_freq=False)
         )
         match_filter_snr = numerator / denominator
         return match_filter_snr
-
-
-
---
+++ viz_con.py
@@ -0,0 +1,72 @@
+from tools import serialize, loadfromjson, prep4Classifier
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+import os
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+import tensorflow as tf
+import numpy as np
+
+# import matplotlib.pyplot as plt
+import sys
+import spsql
+
+s = spsql.spsql()
+
+SCHEMA = os.getenv("SCHEMA", "gw")
+
+# s.curs.execute("select modelhash, savefile,loadfile, weights,cmdline_args from " + SCHEMA + ".runs order by test_accuracy desc limit 100")
+s.curs.execute(
+    "select modelhash, savefile,loadfile, weights,cmdline_args from "
+    + SCHEMA
+    + ".runs where weightshash=%s",
+    (sys.argv[1],),
+)
+
+_weights = s.curs.fetchall()
+(modelhash, savefile, loadfile, weights, cmdline_args) = _weights[0]
+print(savefile, "  -  ", loadfile, "  -  ", cmdline_args)
+print(np.array(weights["weights"][0]).shape)
+print(np.ravel(weights["weights"][0]).shape)
+(x, y, xsig, xema) = prep4Classifier(loadfile, cmdline_args.split(" ")[-1])
+if "run_CNN" in cmdline_args:
+    xem = x
+    print(x.shape)
+else:
+    xem = xema
+(a, c, b) = np.array(weights["weights"][0]).shape
+convweights = np.array(weights["weights"][0])
+
+def mkplot(JK):
+    ze = np.reshape(xem[JK], (xem[JK].size,))
+    re = ze.copy()
+    re[(ze==0)]=1
+    csum = np.convolve(ze, convweights[:, 0, 0], mode="valid")
+    j = 1
+    print(xema)
+    img = []
+    for j in range(1, b):
+        if j < 5:
+            print(convweights[:, 0, j])
+        c = np.convolve(ze, convweights[:, 0, j], mode="valid")
+        csum = csum + c
+        img.append(c/(re[2::]))
+    plt.figure()
+    aimg = np.array(img).reshape(len(img),len(img[0]))
+    plt.imshow(np.log(np.abs(img) + 0.000001))
+    return ze
+
+
+
+
+
+ze=[]
+for K in range(5):
+    ze.append(mkplot(K))
+plt.figure()
+for z in ze:
+    plt.plot(z)
+
+
+plt.show()
